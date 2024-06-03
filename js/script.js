30cb52e816cdb934998e9d18d067267502ced726
shazkun <seanwiltonr@gmail.com>
2024-06-03 20:28:05 +0800

    TEST

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
 // Function to toggle the visibility of the navbar based on the scroll position
 function toggleNavbar() {
-    var section2 = document.getElementById('shownav');
+    var section2 = document.getElementById('about');
     var navbar = document.querySelector('.navbar');
 
     // Get the position of the second section relative to the top of the viewport
@@ -46,25 +46,4 @@ window.addEventListener('load', toggleNavbar);
 // Call the toggleNavbar function when the user scrolls
 window.addEventListener('scroll', toggleNavbar);
 
-
-// window.addEventListener('scroll', function() {
-//     var section2 = document.getElementById('section');
-//     var navbarContainer = document.getElementById('hero-1785');
-
-//     // Get the position of the second section relative to the top of the viewport
-//     var section2Position = section2.getBoundingClientRect().top;
-
-//     // Check if the second section is in the viewport
-//     if (section2Position <= window.innerHeight) {
-//         // Add the navbar HTML to the navbar container
-//         navbarContainer.innerHTML = `
-//             <nav class="navbar navbar-expand-lg navbar-main text-white">
-//                 <!-- Your navbar HTML code -->
-//             </nav>
-//         `;
-        
-//         // Remove the scroll event listener since the navbar has been added
-//         window.removeEventListener('scroll', arguments.callee);
-//     }
-// });
 
