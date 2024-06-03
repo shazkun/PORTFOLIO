9faf160c6f99bed77bcfc20d78ee90a89a7d0991
shazkun <seanwiltonr@gmail.com>
2024-06-03 20:24:24 +0800

    LOAD AND SCROLL JS

--- js/script.js
+++ js/script.js
@@ -24,9 +24,10 @@ $(document).ready(function() {
     $(window).on('scroll', checkVisibility);
 });
 
-window.addEventListener('scroll', function() {
-    var section2 = document.getElementById('about');
-    var navbar = document.querySelector('.navbar-main');
+// Function to toggle the visibility of the navbar based on the scroll position
+function toggleNavbar() {
+    var section2 = document.getElementById('shownav');
+    var navbar = document.querySelector('.navbar');
 
     // Get the position of the second section relative to the top of the viewport
     var section2Position = section2.getBoundingClientRect().top;
@@ -37,5 +38,33 @@ window.addEventListener('scroll', function() {
     } else {
         navbar.classList.add('hidden');
     }
-});
+}
+
+// Call the toggleNavbar function when the page loads
+window.addEventListener('load', toggleNavbar);
+
+// Call the toggleNavbar function when the user scrolls
+window.addEventListener('scroll', toggleNavbar);
+
+
+// window.addEventListener('scroll', function() {
+//     var section2 = document.getElementById('section');
+//     var navbarContainer = document.getElementById('hero-1785');
+
+//     // Get the position of the second section relative to the top of the viewport
+//     var section2Position = section2.getBoundingClientRect().top;
+
+//     // Check if the second section is in the viewport
+//     if (section2Position <= window.innerHeight) {
+//         // Add the navbar HTML to the navbar container
+//         navbarContainer.innerHTML = `
+//             <nav class="navbar navbar-expand-lg navbar-main text-white">
+//                 <!-- Your navbar HTML code -->
+//             </nav>
+//         `;
+        
+//         // Remove the scroll event listener since the navbar has been added
+//         window.removeEventListener('scroll', arguments.callee);
+//     }
+// });
 
